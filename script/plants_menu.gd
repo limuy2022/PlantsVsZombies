620c7da121b366f1f724f0ef9cc67bac532133a8
limuy <limuyang202011@163.com>
2024-07-24 23:29:12 +0800

    ci:format

--- script/plants_menu.gd
+++ script/plants_menu.gd
@@ -4,14 +4,16 @@ var end_index = 518
 var origin_pos
 var prev_pos = 0
 
-func move_up(up_delta:int):
+
+func move_up(up_delta: int):
 	self.position.y = origin_pos - up_delta
 	get_tree().call_group("plants_card", "move_up", up_delta - self.prev_pos)
 	self.prev_pos = up_delta
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	origin_pos = self.position.y
 	var tween = create_tween()
 	tween.tween_method(move_up, 0, end_index, 1.2)
-	tween.tween_callback(func():get_tree().call_group("plants_card", "set_pos"))
+	tween.tween_callback(func(): get_tree().call_group("plants_card", "set_pos"))
